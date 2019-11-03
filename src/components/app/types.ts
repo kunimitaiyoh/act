a6f33a90023e588e6643a2861c067a3327502c4c
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2019-11-03 19:30:59 +0900

    remove ActivityRoute.

--- src/components/app/types.ts
+++ src/components/app/types.ts
@@ -2,6 +2,6 @@ import { FunctionComponent } from "react";
 import { RouteComponentProps } from "react-router";
 
 export interface ActivityDefinition<T extends NonNullable<unknown> = {}> {
-    Component: FunctionComponent<T>;
-    decoder(props: RouteComponentProps<{ [key: string]: string | undefined }>): T | null;
+  decoder(props: RouteComponentProps<{ [key: string]: string | undefined }>): T | null;
+  Component: FunctionComponent<T>;
 }
